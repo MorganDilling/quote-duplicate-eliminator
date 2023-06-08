c4b13022d5ab7bf6953d7bbb902f996775506d06
Morgan Dilling <morgandilling171@gmail.com>
2023-06-08 10:12:40 +0100

    feat: Change how decisions are made, add progress

--- src/App.svelte
+++ src/App.svelte
@@ -3,6 +3,53 @@
   let result: string;
   let similarityPercentage: number = 50;
 
+  let decisionModalShown: boolean = false;
+  let q0: string;
+  let q1: string;
+  let simString: string;
+  let currentDecision: number = 0;
+  let totalDecisions: number = 0;
+
+  const openModal = (): Promise<void> => {
+    return new Promise((resolve) => {
+      decisionModalShown = true;
+      setInterval(() => {
+        const modal = document.getElementById('modal');
+        if (modal) resolve();
+      });
+    });
+  };
+
+  const getDecision = (quote0: string, quote1: string): Promise<0 | 1 | 2> => {
+    return new Promise(async (resolve) => {
+      await openModal();
+      q0 = quote0;
+      q1 = quote1;
+
+      const button0: Element = document.getElementById('0-button');
+      const button1: Element = document.getElementById('1-button');
+      const button2: Element = document.getElementById('2-button');
+
+      button0.addEventListener('click', () => {
+        decisionModalShown = false;
+        button0.removeEventListener('click', () => {});
+        resolve(0);
+      });
+
+      button1.addEventListener('click', () => {
+        decisionModalShown = false;
+        button1.removeEventListener('click', () => {});
+        resolve(1);
+      });
+
+      button2.addEventListener('click', () => {
+        decisionModalShown = false;
+        button2.removeEventListener('click', () => {});
+        resolve(2);
+      });
+    });
+  };
+
   // similarity and editDistance stolen from https://stackoverflow.com/a/36566052/114157
   const editDistance = (s1: string, s2: string): number => {
     s1 = s1.toLowerCase();
@@ -56,30 +103,58 @@
     e.target.value = similarityPercentage;
   };
 
-  const submit = (e: SubmitEvent): void => {
-    const tokens = tokeniseQuotes(quotes);
+  const submit = async (e: SubmitEvent): Promise<void> => {
+    let tokens = tokeniseQuotes(quotes);
     if (!tokens) {
       result = '';
       return;
     }
     const uniqueTokens: string[] = [];
-    for (let i = 0; i < tokens.length; i++) {
-      const token = tokens[i];
-      let isUnique = true;
-      for (let j = 0; j < uniqueTokens.length; j++) {
-        const otherToken = tokens[j];
-        if (i === j) continue;
 
+    while (true) {
+      const token = tokens[0];
+
+      if (!token) break;
+      tokens = tokens.splice(1);
+      const indexesToRemove: number[] = [];
+      let shouldPush = true;
+      currentDecision = 0;
+      totalDecisions = tokens.length;
+      for (let i = 0; i < tokens.length; i++) {
+        currentDecision = i;
+        const otherToken = tokens[i];
         const sim = similarity(token, otherToken);
 
-        if (sim * 100 >= similarityPercentage) {
-          isUnique = false;
-          break;
+        simString = (sim * 100).toFixed(2);
+
+        if (sim === 1) {
+          indexesToRemove.push(i);
+          continue;
+        } else if (sim * 100 >= similarityPercentage) {
+          const decision = await getDecision(token, otherToken);
+          if (decision === 0) {
+            // Keep first
+            indexesToRemove.push(i);
+            continue;
+          } else if (decision === 1) {
+            // Keep second
+            uniqueTokens.push(otherToken);
+            indexesToRemove.push(i);
+            shouldPush = false;
+            break;
+          } else if (decision === 2) {
+            // Keep both
+            uniqueTokens.push(otherToken);
+            indexesToRemove.push(i);
+          }
         }
       }
-      if (isUnique) {
-        uniqueTokens.push(token);
-      }
+
+      tokens = tokens.filter((_, i) => !indexesToRemove.includes(i));
+
+      console.log(tokens);
+
+      if (shouldPush) uniqueTokens.push(token);
     }
 
     result = uniqueTokens.join('\n');
@@ -124,7 +199,7 @@
       </p>
       <p>
         If a comparison of two quotes is more than, or equal to {similarityPercentage}
-        per cent similar, it will not be added to the results.
+        per cent similar, you will be asked what to do.
       </p>
       <textarea
         bind:value={quotes}
@@ -150,8 +225,83 @@
 <p style="position: fixed; left: 0; bottom: 0; padding: 0; margin: 0;">
   Hi Sam :)
 </p>
+{#if decisionModalShown}
+  <div class="decision-modal-bg">
+    <div class="decision-modal" id="modal">
+      <div class="decision-progress">
+        <div class="decision-current">{currentDecision}</div>
+        <div class="decision-slash">/</div>
+        <div class="decision-total">{totalDecisions}</div>
+      </div>
+      <h2>
+        These quotes look similar. <br />What would you like to do?
+        <br />
+        (Similarity: {simString}%)
+      </h2>
+      <p>
+        <b>Quote 1:</b>
+        {q0}
+        <br />
+        <b>Quote 2:</b>
+        {q1}
+      </p>
+      <button id="0-button">Keep first</button>
+      <button id="1-button">Keep second</button>
+      <button id="2-button">Keep both</button>
+    </div>
+  </div>
+{/if}
 
 <style>
+  .decision-progress {
+    position: absolute;
+    top: 5px;
+    right: 5px;
+    width: 40px;
+    height: 40px;
+  }
+
+  .decision-current {
+    position: absolute;
+    top: 0;
+    left: 0;
+  }
+
+  .decision-total {
+    position: absolute;
+    bottom: 0;
+    right: 0;
+  }
+
+  .decision-slash {
+    position: absolute;
+    top: 50%;
+    left: 50%;
+    transform: translate(-50%, -50%);
+  }
+
+  .decision-modal-bg {
+    position: fixed;
+    top: 0;
+    left: 0;
+    width: 100vw;
+    height: 100vh;
+    background: rgba(0, 0, 0, 0.5);
+  }
+
+  .decision-modal {
+    position: fixed;
+    top: 50%;
+    left: 50%;
+    transform: translate(-50%, -50%);
+    background: rgb(61, 61, 61);
+    padding: 1rem;
+    border-radius: 5px;
+    width: 50%;
+    max-width: 500px;
+    text-align: center;
+  }
+
   h1 {
     text-align: center;
   }
@@ -179,5 +329,24 @@
     resize: none;
     width: 100%;
     height: 50px;
+  }
+
+  @media (max-width: 768px) {
+    h1 {
+      font-size: 1.5rem;
+    }
+
+    p {
+      word-wrap: break-word;
+      font-size: 1rem;
+    }
+
+    main {
+      flex-direction: column;
+    }
+
+    main div {
+      width: 100%;
+    }
   }
 </style>
